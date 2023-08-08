e50c649b387f03be4cd6cbdebda748c41fddac08
insidepixce <insidepixce@gmail.com>
2023-08-08 16:39:50 +0900

    네이버 로그인 실습  예제정답

--- naverlogin.py
+++ naverlogin.py
@@ -1,37 +1,39 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 from selenium.webdriver.common.keys import Keys
 import pyperclip
 import time
-#크롬 드라이버 자동업데이를 위함
+
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-#브라우저가 자동으로 꺼질 경우 방지 코드 
+
 chrome_options = Options()
 chrome_options.add_experimental_option('detach', True)
-#크롬 드라이버 최신 버전을 설치 후 서비스 객체를 만듦
+
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
-#웹페이지 오픈을 위한 옵션들
+
 driver.implicitly_wait(5)
 driver.maximize_window()
-#웹페이지 주소로 이동
-driver.get('https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/')
-#아이디 입력
-id = driver.find_element(By.CSS_SELECTOR, '#id')
+
+driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
+
+#ENTER ID
+id= driver.find_element(By.CSS_SELECTOR, "#id")
 id.click()
 pyperclip.copy('insidepixce')
-id.send_keys(Keys.CONTROL, 'v')
+id.send_keys(Keys.CONTROL,'v')
 time.sleep(2)
 
-#비밀번호 입력
-pw = driver.find_element(By.CSS_SELECTOR, '#pw')
+#ENTER PW
+pw = driver.find_element(By.CSS_SELECTOR, "#pw")
 pw.click()
 pyperclip.copy('p8152005!')
-
-pw.send_keys(Keys.CONTROL, 'v')
+pw.send_keys(Keys.CONTROL,'v')
 time.sleep(2)
-login_btn  = driver.find_element(By.CSS_SELECTOR, '#log\.login')
+
+#CLICK LOGIN BUTTON
+login_btn = driver.find_element(By.CSS_SELECTOR, "#log\.login")
 login_btn.click()
