840e8a9858b2e3664ef75c13e35b53e498e504e7
insidepixce <insidepixce@gmail.com>
2023-08-08 17:38:03 +0900

    스택오버플로우 답변 업데이트 + PYPERCLIP으로 수정

--- naverlogin.py
+++ naverlogin.py
@@ -1,39 +1,42 @@
-from selenium import webdriver
+# Import necessary modules and classes from the Selenium library
+from selenium.webdriver import Chrome, ChromeOptions, Keys
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-
-from selenium.webdriver.common.keys import Keys
-import pyperclip
-import time
-
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+import pyperclip
 
-chrome_options = Options()
-chrome_options.add_experimental_option('detach', True)
 
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
+# Configure Chrome options
+chrome_options = ChromeOptions()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
+# Initialize the Chrome driver using the ChromeDriverManager and the configured options
+driver = Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+# Initialize a WebDriverWait object to wait for specific conditions
+wait = WebDriverWait(driver, 10)
 driver.implicitly_wait(5)
 driver.maximize_window()
-
+chrome_options = ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
+# Open the specified URL using the Chrome driver
 driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
 
-#ENTER ID
-id= driver.find_element(By.CSS_SELECTOR, "#id")
-id.click()
+# Find the username input field by its ID and wait until it's present, then input the username
+username = wait.until(EC.presence_of_element_located((By.ID, 'id')))
+#username.send_keys('insidepixce')  # Replace 'your_username' with your actual username
+#time.sleep(10)
 pyperclip.copy('insidepixce')
-id.send_keys(Keys.CONTROL,'v')
-time.sleep(2)
-
-#ENTER PW
-pw = driver.find_element(By.CSS_SELECTOR, "#pw")
-pw.click()
-pyperclip.copy('p8152005!')
-pw.send_keys(Keys.CONTROL,'v')
-time.sleep(2)
-
-#CLICK LOGIN BUTTON
-login_btn = driver.find_element(By.CSS_SELECTOR, "#log\.login")
-login_btn.click()
+username.send_keys(Keys.CONTROL,'v')
+time.sleep(10)
+# Find the password input field by its ID and wait until it's present, then input the password
+password = wait.until(EC.presence_of_element_located((By.ID, 'pw')))
+password.send_keys('p8152005!')  # Replace 'your_password' with your actual password
+time.sleep(10)
+# Simulate pressing the Enter key to submit the login form
+password.send_keys(Keys.ENTER)
+input()
