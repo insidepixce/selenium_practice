ff2ef4179009fddc07bdebec97f3933a0b37efc2
insidepixce <insidepixce@gmail.com>
2023-08-09 22:07:02 +0900

    제2 백업

--- capturethumb_final_updated.py
+++ capturethumb_final_updated.py
@@ -23,6 +23,8 @@ def sanitize_filename(filename):
     return s.strip()
 
 # Initialize
+
+# Initialize
 search_query = input("검색어를 입력하세요: ")
 chrome_options = ChromeOptions()
 chrome_options.add_argument("--start-maximized")
@@ -48,10 +50,9 @@ search_box.send_keys(Keys.RETURN)
 # Wait for loading
 time.sleep(5)
 
-# Scroll to load more videos
 scroll_pause_time = 2
 last_height = driver.execute_script("return document.documentElement.scrollHeight")
-target_video_count = 20
+target_video_count = 10
 
 video_elements = driver.find_elements(By.CSS_SELECTOR, "ytd-video-renderer")
 data = []
@@ -61,7 +62,7 @@ while len(data) < target_video_count:
     
     if not video_elements:
         # Scroll to load more videos
-        print("썸네일 못가져와서 스크롤합니다")
+        print("썸네일을 가져올 수 없어 스크롤합니다")
         driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
         try:
             wait.until(EC.presence_of_element_located((By.ID, "continuations")))
@@ -70,53 +71,37 @@ while len(data) < target_video_count:
         time.sleep(scroll_pause_time)
         continue
 
-for video in video_elements:
-    title = video.find_element(By.ID, "video-title").text
-    sanitized_title = sanitize_filename(title)
-    
-    # 업로더 정보 추출
-    try:
-        uploader_element = video.find_element(By.CSS_SELECTOR, "a.yt-simple-endpoint.style-scope.yt-formatted-string")
-        uploader = uploader_element.text
-    except NoSuchElementException:
-        uploader = "Unknown Uploader"
-    
-    views = video.find_elements(By.CSS_SELECTOR, "span.style-scope.ytd-video-meta-block")[0].text
-    
-    # 썸네일 이미지 요소 찾기
-    try:
-        thumbnail_img_element = video.find_element(By.CSS_SELECTOR, "yt-image img")
-        thumbnail_src = thumbnail_img_element.get_attribute("src")
+    for video in video_elements:
+        title = video.find_element(By.ID, "video-title").text
+        sanitized_title = sanitize_filename(title)
         
-        if thumbnail_src:
-            # 썸네일 저장
-            thumbnail_folder = f"movies/{search_query}/Thumbnail/"
-            safe_create_directory(thumbnail_folder)
-            thumbnail_path = f"{thumbnail_folder}{sanitized_title}.jpg"
-            urllib.request.urlretrieve(thumbnail_src, thumbnail_path)
-            
-            # 데이터 리스트에 저장하여 나중에 엑셀로 내보내기 위함
-            image_filename = f"{sanitized_title}.jpg"
-            data.append([title, uploader, views, image_filename])
-            print(f"('{title}'의 '{image_filename}/{sanitized_title}/{uploader}/{views}' )저장했습니다")
-        else:
-            print(f"('{title}'의 썸네일을 못 가져왔습니다)")
-    except NoSuchElementException:
-        print(f"('{title}'의 썸네일을 못 가져왔습니다)")
+        try:
+            uploader_element = video.find_element(By.CSS_SELECTOR, "a.yt-simple-endpoint.style-scope.yt-formatted-string")
+            uploader = uploader_element.text
+        except NoSuchElementException:
+            uploader = "알 수 없는 업로더"
         
-        # Save thumbnail
-        thumbnail_folder = f"movies/{search_query}/Thumbnail/"
-        safe_create_directory(thumbnail_folder)
-        thumbnail_path = f"{thumbnail_folder}{sanitized_title}.jpg"
-        urllib.request.urlretrieve(thumbnail_src, thumbnail_path)
-        
-        # Save info to a list for later excel export
-        image_filename = f"{sanitized_title}.jpg"
-        data.append([title, uploader, views, image_filename])
-        print(f"('{title}'의 '{image_filename}/{sanitized_title}/{uploader}/{views}' )저장했습니다")
+        views = video.find_elements(By.CSS_SELECTOR, "span.style-scope.ytd-video-meta-block")[0].text
 
-    # Scroll to load more videos
-    print("스크롤합니다")
+        # 이미지 요소가 yt-img 클래스 안에 있을 수 있으므로 yt-img 클래스를 포함한 요소를 찾는다
+        try:
+            thumbnail_img_element = video.find_element(By.CSS_SELECTOR, "yt-img img")
+            thumbnail_src = thumbnail_img_element.get_attribute("src")
+            
+            if thumbnail_src:
+                thumbnail_folder = f"movies/{search_query}/Thumbnail/"
+                safe_create_directory(thumbnail_folder)
+                thumbnail_path = f"{thumbnail_folder}{sanitized_title}.jpg"
+                urllib.request.urlretrieve(thumbnail_src, thumbnail_path)
+                print(f"('{title}'의 '{thumbnail_path}/{sanitized_title}/{uploader}/{views}' )를 저장했습니다")
+            else:
+                print(f"('{title}')의 썸네일을 가져올 수 없습니다")
+            
+            data.append([title, uploader, views, thumbnail_path])
+
+        except NoSuchElementException:
+            print(f"('{title}'의 썸네일을 가져올 수 없습니다")
+
     driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
     try:
         wait.until(EC.presence_of_element_located((By.ID, "continuations")))
@@ -124,49 +109,41 @@ for video in video_elements:
         break
     time.sleep(scroll_pause_time)
 
-# Save info to text file
 info_folder = f"movies/{search_query}/INFO/"
 safe_create_directory(info_folder)
 info_file_path = f"{info_folder}{search_query}.txt"
+
 with open(info_file_path, "w", encoding="utf-8") as info_file:
     for item in data:
         info_file.write(f"Title: {item[0]}\n")
         info_file.write(f"Uploader: {item[1]}\n")
         info_file.write(f"Views: {item[2]}\n")
-        info_file.write(f"Image Filename: {item[3]}\n\n")
+        info_file.write(f"Thumbnail: {item[3]}\n\n")
 
-# Save to Excel
-df = pd.DataFrame(data, columns=["Title", "Uploader", "Views", "Image Filename"])
+df = pd.DataFrame(data, columns=["Title", "Uploader", "Views", "Thumbnail"])
 
-# Save to Excel with images
 excel_folder = f"movies/{search_query}_excel/"
 safe_create_directory(excel_folder)
 excel_path = f"{excel_folder}{search_query}.xlsx"
 
-# Create a new Excel workbook
 workbook = pd.ExcelWriter(excel_path, engine='xlsxwriter')
 df.to_excel(workbook, index=False, sheet_name='Videos')
 
-# Get the xlsxwriter workbook and worksheet objects
 workbook = workbook.book
 worksheet = workbook.get_worksheet_by_name('Videos')
 
-# Get the dimensions of the DataFrame
 max_row = df.shape[0]
 max_col = df.shape[1]
 
-# Add the image column
-worksheet.set_column('E:E', 20)  # Adjust the width of the image column
+worksheet.set_column('E:E', 20)
 
-# Add images to the Excel file
 for row_num in range(1, max_row + 1):
-    image_path = df.loc[row_num - 1, 'Image Filename']
-    img_path = f"movies/{search_query}/Thumbnail/{image_path}"
-    worksheet.insert_image(row_num, max_col, img_path, {'x_scale': 0.5, 'y_scale': 0.5})  # Adjust the scale
+    image_path = df.loc[row_num - 1, 'Thumbnail']
+    worksheet.insert_image(row_num, max_col, image_path, {'x_scale': 0.5, 'y_scale': 0.5})
 
 workbook.close()
 
-print(f"All data saved. Excel file path: {excel_path}")
+print(f"모든 데이터가 저장되었습니다. 엑셀 파일 경로: {excel_path}")
 
 # Close browser
 driver.quit()
